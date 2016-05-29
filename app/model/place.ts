7b48006c380be67652d6738b1b6e9b19505f942e
olanhect <olanhector@gmail.com>
2016-05-28 23:40:40 -0400

    Implement comparator

--- app/model/place.ts
+++ app/model/place.ts
@@ -11,4 +11,29 @@ export class Place {
     userId: number;
     links: Links;
     dateModified: string;
+    
+    static equal(p1: Place, p2: Place): boolean{
+        if (p1.name != p2.name) {
+            return false;
+        }
+        if (p1.city != p2.city){
+            return false;
+        }
+        if (p1.image != p2.image){
+            return false;
+        }
+        if (p1.category != p2.category){
+            return false;
+        }
+        if (p1.type != p2.type){
+            return false;
+        }
+        if (p1.phoneNumber != p2.phoneNumber){
+            return false;
+        }
+        if (p1.userId != p2.userId){
+            return false;
+        }
+        return true;
+    }
 }
