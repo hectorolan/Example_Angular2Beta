c1897547a6028805f05a9c8956289734b28f941e
olanhect <olanhector@gmail.com>
2016-05-28 23:39:48 -0400

    Add write message, change error to type any

--- app/service/log.ts
+++ app/service/log.ts
@@ -3,18 +3,40 @@ export class Log {
     static console: boolean = true;
     static db: boolean = false;
     
-    public static writeError(message: string){
-        if (this.console) {
-            this.writeConsoleError(message);
-        }
-        if (this.db) {
-            this.writeDbError(message);
-        }
+    /* Write Regular Message
+     * */
+    public static writeMessage(message: any){
+        this._writeConsoleMessage(message);
+        this._writeDbMessage(message);
     }
-    static writeConsoleError(message: string){
+    static _writeConsoleMessage(message: any){
+        if (!this.console){
+            return;
+        }
+        console.log(message);
+    }
+    static _writeDbMessage(message: any){
+        if (!this.db){
+            return;
+        }
+        
+    }
+    /* Write Error
+     * */
+    public static writeError(message: any){
+        this._writeConsoleError(message);
+        this._writeDbError(message);
+    }
+    static _writeConsoleError(message: any){
+        if (!this.console){
+            return;
+        }
         console.error(message);
     }
-    static writeDbError(message: string){
+    static _writeDbError(message: any){
+        if (!this.db){
+            return;
+        }
         
     }
 }
