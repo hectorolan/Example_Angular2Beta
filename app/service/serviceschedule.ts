8d328eced90150564c7841a0012133507ec1895f
olanhect <olanhector@gmail.com>
2016-05-28 23:39:14 -0400

    Services implements add, update Schedule

--- app/service/serviceschedule.ts
+++ app/service/serviceschedule.ts
@@ -6,56 +6,62 @@ import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class ServiceSchedule {
-    path: string = "http://localhost:8080/restful-pam/webapi/place/";
-    
+    path: string = "http://localhost:8080/restful-pam/webapi/place";
+
     constructor(
         private http: Http
     ) {
     }
-    /*
-     * Get place schedule*/
+    /* Get place schedule */
     getSchedule(placeId: number): Observable<Schedule> {
         let options = this.getOptions();
-        return this.http.get(this.path + placeId + "/schedule", options)
+        let path = this.path + "/" + placeId + "/schedule";
+        return this.http.get(path, options)
             .map(this.extractData)
             .catch(this.handleError);
     }
-    /*
-     * Save new schedule*/
-    addSchedule(schedule: Schedule): Observable<Schedule> {
-        //TODO
-        let body = JSON.stringify({ schedule });
+    /* Save new Schedule */
+    addSchedule(pladeId: number, schedule: Schedule): Observable<Schedule> {
+        let body = JSON.stringify(schedule);
         let options = this.getOptions();
-        return this.http.post(this.path, body, options)
+        let path = this.path + "/" + pladeId + "/schedule";
+        Log.writeMessage(body);
+        return this.http.post(path, body, options)
             .map(this.extractData)
             .catch(this.handleError);
     }
-    /*
-     * Set Header */
-    private getOptions(): RequestOptions{
-        let headers = new Headers({ 
-                'Content-Type': 'application/json'
-            });
-        headers.append("Authorization","Basic " + btoa("hector"+":"+"olan"))
+    /* Update Schedule */
+    updateSchedule(schedule: Schedule): Observable<Schedule> {
+        let body = JSON.stringify(schedule);
+        let option = this.getOptions();
+        let path = this.path + "/" + schedule.placeId + "/schedule";
+        return this.http.put(path, body, option)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+    /* Set Header */
+    private getOptions(): RequestOptions {
+        let headers = new Headers({
+            'Content-Type': 'application/json'
+        });
+        headers.append("Authorization", "Basic " + btoa("hector" + ":" + "olan"))
         let options = new RequestOptions({ headers: headers });
         return options;
     }
-    /*
-     * Get Request*/
+    /* Get Request */
     private extractData(response: Response) {
         if (response.status < 200 || response.status >= 300) {
             try {
-            throw new Error('E:' + response.status);
-            }catch(
-                ex
-            ){}
+                throw new Error('E:' + response.status);
+            } catch (
+            ex
+            ) { }
         }
         let body = response.json();
         Log.writeError(body);
         return body || {};
     }
-    /*
-     * Error*/
+    /* Error */
     private handleError(error: any) {
         let message = error.message || 'Server error';
         Log.writeError(message);
